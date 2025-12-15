fd598ae334d082797053fcc07086c219ba900f8d
hammadrehmandev <hammad.rehman@vativeapps.com>
2025-12-15 12:09:16 +0500

    learning ci cd

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World from CI/CD 🚀" });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
